fc9834fb5185d1e74955da03d7205c3b3895bd66
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-03-23 23:19:50 +0700

    s4-define-scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Builder;
 class Book extends Model
 {
     use HasFactory;
@@ -12,5 +12,10 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+    // add this we can use Book::title('title')->get();
+    public function scopeTitle(Builder $query, string $title)
+    {
+        return $query->where('title', 'like', '%'.$title.'%');
     }
 }
